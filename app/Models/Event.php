6194ec36c3f8a433c9e14a4c0f9426fb4e962240
hamzashiwani <hm4465496@gmail.com>
2024-10-01 12:11:45 -0700

    some changes

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Hotel;
+use App\Models\Flight;
+use App\Models\Transport;
 
 class Event extends Model
 {
