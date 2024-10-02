9a0dc901712a7daf6a1489938f66cea8b59562fa
hamzashiwani <hm4465496@gmail.com>
2024-10-02 16:07:40 -0700

    some changes

--- app/Models/EventListing.php
+++ app/Models/EventListing.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class EventListing extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $appends = ['is_attendance'];
+
+    function getIsAttendanceAttribute() {
+        $check = EventAttendance::where('user_id',Auth::id())->where('event_attendance_id',$this->id)->first();
+        if($check) {
+            return true;
+        }
+        return false;
+    }
 }
