55bf21c9687c88d1c44ad3beebcdb75d756df8be
hamzashiwani <107464427+hamzashiwani@users.noreply.github.com>
2024-10-15 18:37:27 +0500

    Update index.blade.php

--- resources/views/admin/hotel/index.blade.php
+++ resources/views/admin/hotel/index.blade.php
@@ -60,6 +60,28 @@
             </div>
         </div>
     </section>
+   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="exampleModalLabel">Import Hotels</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form id="formAdd" method="post" name="formAdd" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body">
+                        <input type="file" id="file" name="file" class="form-control csv--file">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('footer-js')
