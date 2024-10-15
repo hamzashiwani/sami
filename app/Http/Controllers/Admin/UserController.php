2f5ca72c86cbe8c07d86687f1bc19c5786cf4599
hamzashiwani <107464427+hamzashiwani@users.noreply.github.com>
2024-10-15 19:54:36 +0500

    Update UserController.php

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
 
 
 class UserController extends Controller
