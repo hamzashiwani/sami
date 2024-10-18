f43b6550dfc5adb66d21a2d71e53d03035f9b3b7
hamzashiwani <107464427+hamzashiwani@users.noreply.github.com>
2024-10-18 14:39:37 +0500

    Update form.blade.php

--- resources/views/admin/group/form.blade.php
+++ resources/views/admin/group/form.blade.php
@@ -117,6 +117,12 @@
                     let option1 = new Option(user.name, user.id, false, false);
                     $('#user-dropdown').append(option1);
 
+                    @if(isset($data->cordinator_id))
+                        if ({{ json_encode($data->cordinator_id) }}.includes(user.id)) {
+                            option1.selected = true; // Mark as selected if exists in $data->members
+                        }
+                    @endif
+
                     let option2 = new Option(user.name, user.id, false, false);
                     $('#user-dropdown1').append(option2);
 
