8682c793fa2f4a43a75a8099b0b5329fba2be374
Khurram Munir <kmunir38@gmail.com>
2024-10-22 04:15:44 +0500

    Added Validations & columns employee_id & passport_number in users

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -18,13 +18,13 @@
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="first_name"> Name *</label>
-                                            <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+                                            <input type="text" name="name" value="{{ old('name') }}" class="form-control" required>
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="first_name"> Designation *</label>
-                                            <input type="text" name="designation" value="{{ old('designation') }}" class="form-control">
+                                            <input type="text" name="designation" value="{{ old('designation') }}" class="form-control" required>
                                         </div>
                                     </div>
                                 </div>
@@ -33,14 +33,30 @@
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="phone">Contact Number *</label>
+                                            <label for="employee_id"> Employee ID *</label>
+                                            <input type="text" name="employee_id" value="{{ old('employee_id') }}" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="passport_number"> Passport Number *</label>
+                                            <input type="text" name="passport_number" value="{{ old('passport_number') }}" class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                            <fieldset>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label for="phone">Contact Number</label>
                                             <input type="number" name="phone" value="{{ old('phone') }}" class="form-control">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="email">Email *</label>
-                                            <input type="email" name="email" value="{{ old('email') }}" class="form-control">
+                                            <input type="email" name="email" value="{{ old('email') }}" class="form-control" required>
                                         </div>
                                     </div>
                                 </div>
@@ -50,12 +66,12 @@
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label for="phone">Division *</label>
-                                            <input type="text" name="division" value="{{ old('division') }}" class="form-control">
+                                            <input type="text" name="division" value="{{ old('division') }}" class="form-control" required>
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label for="email">Base Town *</label>
+                                            <label for="email">Base Town</label>
                                             <input type="text" name="base_town" value="{{ old('base_town') }}" class="form-control">
                                         </div>
                                     </div>
