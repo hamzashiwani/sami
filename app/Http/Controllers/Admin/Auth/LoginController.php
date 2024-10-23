ec994c4891ff38b2a79e172ffccc1b6dc2025dbb
hamzashiwani <107464427+hamzashiwani@users.noreply.github.com>
2024-10-23 17:01:18 +0500

    Update LoginController.php

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -61,15 +61,6 @@ class LoginController extends Controller
         $this->validateLogin($request);
 
         if ($this->attemptLogin($request)) {
-            if (Auth::user()->type == 2) {
-                Auth::logout(); // Log the user out
-                return redirect()
-                    ->back()
-                    ->withErrors([
-                        'email' => 'Access denied. Your account is not allowed to log in.',
-                    ]);
-            }
-    
             return $this->sendLoginResponse($request);
         }
 
