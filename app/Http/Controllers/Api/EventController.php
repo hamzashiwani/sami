2ec0082caf6f67605ffab5739d7629effe1ddad6
Khurram Munir <kmunir38@gmail.com>
2024-10-25 05:51:46 +0500

    Fixed Issue of Transport

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -155,9 +155,9 @@ class EventController extends BaseController
             // Get leaderboard data
             $leaderboard['leadorboard'] = SubmitAnswer::select('user_id', 'quiz_id')
                 ->where('quiz_id', $event->id)
-                ->selectRaw('MIN(seconds) as total_time, COUNT(CASE WHEN is_correct THEN 1 END) as total_correct_answers')
+                ->selectRaw('SUM(time_remain) as total_time, COUNT(CASE WHEN is_correct THEN 1 END) as total_correct_answers')
                 ->groupBy('user_id')
-                ->orderBy('points', 'asc') // Assuming you want to order by time ascending (faster is better)
+                ->orderBy('total_time', 'asc') // Assuming you want to order by time ascending (faster is better)
                 ->with('user') // Assuming you want user details as well
                 ->get();
             } else {
@@ -284,11 +284,11 @@ class EventController extends BaseController
                     if($event->correct_answer == $request->answer) {
                         $points = $request->seconds + 5;
                         $correct = 1;
-                        $time_remain = $main->question_time - $request->seconds;
+                        $time_remain = $request->seconds;
                     } else {
                         $points = 0;
                         $correct = 0;
-                        $time_remain = $main->question_time - $request->seconds;
+                        $time_remain = $request->seconds;
                     }
                     $getUserData = [
                         'user_id' => auth()->user()->id,
@@ -296,8 +296,7 @@ class EventController extends BaseController
                         'quiz_id' => $main->id,
                         'question_id' => $request->question_id,
                         'answer' => $request->answer,
-                        'seconds' => $request->seconds,
-                        'points' => $points,
+                        'seconds' => $points,
                         'is_correct' => $correct,
                         'time_remain' => $time_remain,
                     ];              
